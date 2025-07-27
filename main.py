8e9dcbf9b9877d161e1a88a753fca2b83a0e3e00
THScoach <swingrehabcoach@gmail.com>
2025-07-26 21:11:55 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return '<h1>The Hitting Skool - Coming Soon!</h1><p>Your application is successfully deployed!</p>'
+
+if __name__ == '__main__':
+    app.run()
